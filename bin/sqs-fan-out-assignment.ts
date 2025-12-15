3bd5ff82b10a941c00c6b817ba0b9a0d36c26007
Ben Trebilcock <bentrebilcock14@outlook.com>
2025-12-14 23:19:00 -0600

    Initial commit

--- bin/sqs-fan-out-assignment.ts
+++ bin/sqs-fan-out-assignment.ts
@@ -4,17 +4,9 @@ import { SqsFanOutAssignmentStack } from '../lib/sqs-fan-out-assignment-stack';
 
 const app = new cdk.App();
 new SqsFanOutAssignmentStack(app, 'SqsFanOutAssignmentStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+  description: 'Image processing pipeline with S3, SNS, SQS, and Lambda',
 });
--- lib/sqs-fan-out-assignment-stack.ts
+++ lib/sqs-fan-out-assignment-stack.ts
@@ -1,16 +1,142 @@
-import * as cdk from 'aws-cdk-lib/core';
+import * as cdk from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as sns from 'aws-cdk-lib/aws-sns';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as s3n from 'aws-cdk-lib/aws-s3-notifications';
+import * as subscriptions from 'aws-cdk-lib/aws-sns-subscriptions';
+import * as lambdaEventSources from 'aws-cdk-lib/aws-lambda-event-sources';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class SqsFanOutAssignmentStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    //Input Bucket
+    const inputBucket = new s3.Bucket(this, 'InputBucket', {
+      bucketName: `image-input-bucket-${cdk.Aws.ACCOUNT_ID}`,
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // Delete bucket when stack is destroyed (dev only!)
+      autoDeleteObjects: true, // Delete objects when bucket is destroyed (dev only!)
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      versioned: false,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'SqsFanOutAssignmentQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    //Output Bucket
+    const outputBucket = new s3.Bucket(this, 'OutputBucket', {
+      bucketName: `image-input-bucket-${cdk.Aws.ACCOUNT_ID}-resized`,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      versioned: false,
+    });
+
+    //SNS Topic
+    const imageTopic = new sns.Topic(this, 'ImageProcessingTopic', {
+      displayName: 'Image Processing Topic',
+      topicName: 'image-processing-topic',
+    });
+
+    //SQS Queue
+    const imageQueue = new sqs.Queue(this, 'ImageProcessingQueue', {
+      queueName: 'image-processing-queue',
+      visibilityTimeout: cdk.Duration.seconds(300), // Must be >= Lambda timeout
+      retentionPeriod: cdk.Duration.days(4),
+    });
+
+    //Subscribe SQS to SNS
+    imageTopic.addSubscription(new subscriptions.SqsSubscription(imageQueue));
+
+    //S3 Event Notifications
+    // For .jpg files
+    inputBucket.addEventNotification(
+      s3.EventType.OBJECT_CREATED,
+      new s3n.SnsDestination(imageTopic),
+      { suffix: '.jpg' }
+    );
+
+    // For .jpeg files
+    inputBucket.addEventNotification(
+      s3.EventType.OBJECT_CREATED,
+      new s3n.SnsDestination(imageTopic),
+      { suffix: '.jpeg' }
+    );
+
+    // For .png files
+    inputBucket.addEventNotification(
+      s3.EventType.OBJECT_CREATED,
+      new s3n.SnsDestination(imageTopic),
+      { suffix: '.png' }
+    );
+
+
+    //Lambda Function
+    const imageProcessorFunction = new lambda.Function(this, 'ImageProcessorFunction', {
+      functionName: 'image-processor',
+      runtime: lambda.Runtime.PYTHON_3_11,
+      handler: 'lambda_function.lambda_handler', // file.function
+      code: lambda.Code.fromAsset('lambda'), // Folder with lambda_function.py and python/
+      timeout: cdk.Duration.seconds(300), // 5 minutes max
+      memorySize: 512, // MB - increase if processing large images
+      environment: {
+        INPUT_BUCKET: inputBucket.bucketName,
+        OUTPUT_BUCKET: outputBucket.bucketName,
+        VERSION: '2',
+      },
+      description: 'Processes uploaded images and creates 128x128 thumbnails',
+    });
+
+
+
+    const pillowLayer = new lambda.LayerVersion(this, 'PillowLayer', {
+      code: lambda.Code.fromAsset('lambda-layer'),
+      compatibleRuntimes: [lambda.Runtime.PYTHON_3_11],
+      description: 'Pillow (PIL) for image processing',
+    });
+
+    imageProcessorFunction.addLayers(pillowLayer);
+
+    //Lambda Permissions
+    inputBucket.grantRead(imageProcessorFunction);
+
+    outputBucket.grantWrite(imageProcessorFunction);
+
+    //SQS Trigger Lambda
+    imageProcessorFunction.addEventSource(
+      new lambdaEventSources.SqsEventSource(imageQueue, {
+        batchSize: 10, // Process up to 10 messages at once
+        maxBatchingWindow: cdk.Duration.seconds(5), // Wait up to 5 seconds to fill batch
+        reportBatchItemFailures: true, // Only delete successfully processed messages
+      })
+    );
+
+    //Stack Outputs
+    new cdk.CfnOutput(this, 'InputBucketName', {
+      value: inputBucket.bucketName,
+      description: 'S3 bucket for uploading images',
+      exportName: 'ImageInputBucket',
+    });
+
+    new cdk.CfnOutput(this, 'OutputBucketName', {
+      value: outputBucket.bucketName,
+      description: 'S3 bucket where resized images will be stored',
+      exportName: 'ImageOutputBucket',
+    });
+
+    new cdk.CfnOutput(this, 'SNSTopicArn', {
+      value: imageTopic.topicArn,
+      description: 'SNS Topic ARN for image processing events',
+    });
+
+    new cdk.CfnOutput(this, 'SQSQueueUrl', {
+      value: imageQueue.queueUrl,
+      description: 'SQS Queue URL for image processing',
+    });
+
+    new cdk.CfnOutput(this, 'LambdaFunctionName', {
+      value: imageProcessorFunction.functionName,
+      description: 'Lambda function name for image processing',
+    });
   }
 }
